babdbc2787cae631b676dd5eb7744c6badc44f7a
Jason Christian <jasonchristian1234@gmail.com>
2026-02-12 11:55:45 +1300

    feat: add background color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 98, 71, 146),
         body: Center(
           child: Text('Hello World'),
         ),
