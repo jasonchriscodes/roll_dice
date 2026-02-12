f1d45a1b5e23929d37047f734d9b403269d800ae
Jason Christian <jasonchristian1234@gmail.com>
2026-02-12 18:50:07 +1300

    feat: add gradient color

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 98, 71, 146),
-        body: Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98)
+            ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     ),
